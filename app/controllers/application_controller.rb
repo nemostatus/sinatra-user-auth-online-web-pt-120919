46b8979d86881cfebfeb2b7b048e692b114875d5
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-03-29 16:12:48 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :views, Proc.new { File.join(root, "../views/") }
@@ -8,6 +9,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+
     erb :home
   end
 
@@ -17,12 +19,15 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:user_id] = @user.id
+
+@user = User.new(name: params["name"], email: params["email"], password: params["password"])
+
+@user.save
+session[:user_id] = @user.id
 
     redirect '/users/home'
   end
+
 
   get '/sessions/login' do
 
@@ -31,22 +36,24 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.find_by(email: params[:email], password: params[:password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/users/home'
-    end
-    redirect '/sessions/login'
-  end
 
+    @user = User.find_by(email: params["email"], password: params["password"])
+ 
+    session[:user_id] = @user.id
+
+  redirect "/users/home"
+ 
+  
+end
+    
   get '/sessions/logout' do
     session.clear
-    redirect '/'
+   redirect '/'
   end
-
-  get '/users/home' do
-
-    @user = User.find(session[:user_id])
+  
+      get '/users/home' do
+ @user = User.find(session[:user_id])
     erb :'/users/home'
+    end
   end
-end
+
